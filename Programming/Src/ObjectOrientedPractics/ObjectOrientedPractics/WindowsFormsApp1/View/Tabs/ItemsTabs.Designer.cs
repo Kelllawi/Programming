7826f14605560744cdb675675d9a94f6ee4d80be
Костя Марков <kelllawimk@mail.ru>
2023-01-11 05:05:51 +0700

    1

--- Programming/Src/ObjectOrientedPractics/ObjectOrientedPractics/WindowsFormsApp1/View/Tabs/ItemsTabs.Designer.cs
+++ Programming/Src/ObjectOrientedPractics/ObjectOrientedPractics/WindowsFormsApp1/View/Tabs/ItemsTabs.Designer.cs
@@ -28,18 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ItemsListBox = new System.Windows.Forms.ListBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.ItemsLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // ItemsListBox
+            // 
+            this.ItemsListBox.FormattingEnabled = true;
+            this.ItemsListBox.Location = new System.Drawing.Point(6, 22);
+            this.ItemsListBox.Name = "ItemsListBox";
+            this.ItemsListBox.Size = new System.Drawing.Size(285, 433);
+            this.ItemsListBox.TabIndex = 1;
+            this.ItemsListBox.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(6, 461);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(90, 50);
+            this.AddButton.TabIndex = 2;
+            this.AddButton.Text = "button1";
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Location = new System.Drawing.Point(102, 461);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(90, 50);
+            this.RemoveButton.TabIndex = 3;
+            this.RemoveButton.Text = "button2";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            // 
+            // ItemsLabel
+            // 
+            this.ItemsLabel.AutoSize = true;
+            this.ItemsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ItemsLabel.Location = new System.Drawing.Point(3, 6);
+            this.ItemsLabel.Name = "ItemsLabel";
+            this.ItemsLabel.Size = new System.Drawing.Size(37, 13);
+            this.ItemsLabel.TabIndex = 4;
+            this.ItemsLabel.Text = "Items";
             // 
             // ItemsTabs
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ItemsLabel);
+            this.Controls.Add(this.RemoveButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.ItemsListBox);
             this.Name = "ItemsTabs";
-            this.Size = new System.Drawing.Size(727, 519);
+            this.Size = new System.Drawing.Size(800, 530);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.ListBox ItemsListBox;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button RemoveButton;
+        private System.Windows.Forms.Label ItemsLabel;
     }
 }
--- Programming/Src/ObjectOrientedPractics/ObjectOrientedPractics/WindowsFormsApp1/View/Tabs/ItemsTabs.cs
+++ Programming/Src/ObjectOrientedPractics/ObjectOrientedPractics/WindowsFormsApp1/View/Tabs/ItemsTabs.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp1.View.Tabs
         {
             InitializeComponent();
         }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
