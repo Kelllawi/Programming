8ff5f876ec0b134d51feb971b1e3a2ed8dc2d383
Костя Марков <kelllawimk@mail.ru>
2023-01-11 04:57:24 +0700

    добавление классов и валидаций

---
+++ Programming/Src/ObjectOrientedPractics/ObjectOrientedPractics/WindowsFormsApp1/Model/Customers.cs
@@ -0,0 +1,55 @@
+﻿using ObjectOrientedPractics.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    internal class Customers
+    {
+        private readonly int _customersId;
+        private  string _fullName;
+        private  string _address;
+
+        public int Id
+        {
+            get { return _customersId; }
+        }
+
+        private string FullName
+        {
+            get { return _fullName; }
+            set
+            {
+                Validator.AssertStringOnLength(FullName, 200, nameof(FullName));
+                _fullName= value;
+            }
+        }
+        private string Address
+        { get { return _address; } 
+            set
+            {
+                Validator.AssertStringOnLength(Address, 500, nameof(Address));
+                _address= value;
+            }
+        }
+
+        public Customers(int id,string fullName, string address)
+        {
+            _customersId= IdGenerator.GetNextCustomersId();
+            fullName = _fullName;
+            address = _address;
+
+        }
+        public Customers()
+        {
+            _customersId= IdGenerator.GetNextCustomersId();
+            FullName= "FullName";
+            Address = "Address";
+        }
+
+
+    }
+}
